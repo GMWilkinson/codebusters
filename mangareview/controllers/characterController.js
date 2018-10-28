119514a4a1cf082ceb1ef7d0a14118500d77ee0a
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-10-28 20:59:25 +0000

    added basic logic

---
+++ mangareview/controllers/characterController.js
@@ -0,0 +1,19 @@
+// const Manga = require('../models/manga');
+//
+//
+//
+//
+// function createRoute(req, res) {
+//   Manga.findById(req.params.mangaId)
+//     .then(manga => {
+//       console.log('Creating a character', manga, req.body);
+//       // Add a rating to the cocktail's ratings array
+//       manga.characters.push(req.body);
+//       manga.save().then(() => res.redirect('/mangas'));
+//     });
+// }
+//
+//
+// module.exports = {
+//   createRoute: createRoute
+// };
